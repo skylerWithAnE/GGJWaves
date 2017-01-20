a36b203e9a161101e42ac857373d3ec0d786910d
Skyler Evans <Skyler Evans>
2017-01-20 18:24:58 -0500

    initial

---
+++ Source/Waves/WavesGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "WavesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WAVES_API AWavesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
